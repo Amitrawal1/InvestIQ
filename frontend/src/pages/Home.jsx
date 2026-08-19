92cccf3e1bc63e24fb4df0a7ae484576d92bcd30
AMIT Rawal <119728577+Amitrawal1@users.noreply.github.com>
2026-08-19 17:00:04 +0530

    install libray with full setup

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <>
+        <div className="home-container bg-gray-400 ">   
+            <h1>Welcome to InvestIQ</h1>
+            <p>Your one-stop solution for investment insights and portfolio management.</p>
+        </div>
+    </>
+    
+    
+  );
+}
